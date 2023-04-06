8c971d90e32b43d8727b5e4ad3df0be9b6f75dd9
Pengineer <dotsenko.sar@gmail.com>
2023-04-06 16:57:35 +0400

    Lab4
    SysTick_Config(SystemCoreClock /1000);//1ms

--- Lab4/User/main.c
+++ Lab4/User/main.c
@@ -1,5 +1,6 @@
 #include "stm32f4xx.h"  // Device header
 #include "main.h"
+#include "Lab4_Test.h"
 
 #define SYSCLOC 168000000U
 
@@ -7,11 +8,31 @@
 #define TIM_EnableCounter(TIMx) SET_BIT(TIMx->CR1, TIM_CR1_CEN)
 #define TIM_DisableCounter(TIMx) CLEAR_BIT(TIMx-> CR1, TIM_CR1_CEN)
 
+__IO uint32_t SysTick_CNT = 0;
+__IO uint8_t TIM2_count =0;
+uint8_t TestVar;
+uint32_t Test;
+uint8_t *data;
+
+void SysTick_Init(void)
+{
+	MODIFY_REG(SysTick->LOAD,SysTick_LOAD_RELOAD_Msk, SYSCLOC/1000-1);
+	CLEAR_BIT(SysTick->VAL,SysTick_VAL_CURRENT_Msk);
+	SET_BIT(SysTick->CTRL, SysTick_CTRL_CLKSOURCE_Msk |SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_TICKINT_Msk);
+}
+
+void delay_ms(uint32_t ms)
+{
+	MODIFY_REG(SysTick->VAL, SysTick_VAL_CURRENT_Msk, SYSCLOC/1000 - 1);
+	SysTick_CNT = ms;
+	while(SysTick_CNT) {}
+}
+
 static void TIM2_Init(void)
 {
 	SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM2EN);
 	NVIC_EnableIRQ(TIM2_IRQn);
-	WRITE_REG(TIM2->PSC, 3599);
+	WRITE_REG(TIM2->PSC, 8399);
 	WRITE_REG(TIM2->ARR, 2000);
 }
 
@@ -44,41 +65,6 @@ void SetSysClockTo168(void)
 }
 
 
-//uint8_t TestVar;
-//uint32_t Test;
-uint8_t *decision;
-uint8_t tim6_count = 0;
-uint8_t User_Button = 0;
-uint16_t Count_Of_Button = 0;
-uint8_t Button_State = 0;
-uint8_t Flag = 0;
-int Rattle_Of_Button(void); 
-
-int Rattle_Of_Button(void) 
-			{
-				if(User_Button == Bit_SET) 
-          {
-						if(Count_Of_Button < 1700)
-						{
-							Count_Of_Button++;
-						}else {
-							if(User_Button == Bit_SET) {
-								User_Button = Bit_SET;
-								return 1;
-							}
-						}
-					}else {
-						if(Count_Of_Button > 0) 
-							{
-								Count_Of_Button--;
-							}else {
-								User_Button = Bit_RESET;
-								return 0;
-							}
-					}
-			}
-
-
 int main(void)
 	{
 		GPIO_InitTypeDef GPIO_struct_LED; //инициализируем переменную GPIO_struct_LED с типом структуры GPIO_InitTypeDef
@@ -94,83 +80,91 @@ int main(void)
 		
 		GPIO_Init(GPIOD, &GPIO_struct_LED); //инициализируем порт D
 		
-		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); //включаем тактирование порта A
-		
-		GPIO_struct_Button.GPIO_Pin = GPIO_Pin_0; //конфигурируем структуру с параметрами на ножке PA0
-		GPIO_struct_Button.GPIO_Mode = GPIO_Mode_IN; //включаем ножку PA0 на вход
-		GPIO_struct_Button.GPIO_Speed = GPIO_Low_Speed; //установим скорость LOW на конфигурируемые ножки порта*/
-		GPIO_struct_Button.GPIO_OType = GPIO_OType_PP; //включем одтягивающее сопротивление на весь порт
-		GPIO_struct_Button.GPIO_PuPd = GPIO_PuPd_NOPULL;//этот параметр используется при включении порта на вход, поэтому установили его по умолчанию
-		
-		GPIO_Init(GPIOA, &GPIO_struct_Button); //инициализируем порт A
-		
+				
 		TIM2_Init();
 		TIM_EnableIT_UPDATE(TIM2);
 		TIM_EnableCounter(TIM2);
 
-		//TestVar = Lab3_Test_ini("Dotsenko");
+		TestVar = Lab4_Test_ini("Dotsenko");
 
-		//SystemCoreClockUpdate();
-		
-				
+						
 		RCC_ClocksTypeDef RCC_Clocks1;
 		
 				
 		SetSysClockTo168();
+		//SysTick_Init();
+		SysTick_Config(SystemCoreClock /1000);//1ms
 		
 
-		
+		SystemCoreClockUpdate();
 		RCC_GetClocksFreq(&RCC_Clocks1);
 		
 		while(1)
 		{
-			//Test = while_Test(decision);
-			User_Button = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
-			Button_State = Rattle_Of_Button();
 			
-				if(Button_State == Bit_SET)
-					{
-						if(Flag == 0)
-						{
-							Flag = 1;
-						switch(tim6_count)
+			switch(TIM2_count)
 						{
 							case 0:
+								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
+								GPIO_SetBits(GPIOD, GPIO_Pin_12); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD12
+							  delay_ms(4100);
+							  break;
 							case 3:
+								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
+								GPIO_SetBits(GPIOD, GPIO_Pin_12); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD12
+							  delay_ms(200);
+							  break;
 							case 5:	
 								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
 								GPIO_SetBits(GPIOD, GPIO_Pin_12); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD12
+							  delay_ms(2000);
 							  break;
 							case 1:
+								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
+								GPIO_SetBits(GPIOD, GPIO_Pin_13); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD13
+							  delay_ms(100);
+							  break;
 							case 6:
 								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
 								GPIO_SetBits(GPIOD, GPIO_Pin_13); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD13
+							  delay_ms(3800);
 							  break;
 							case 2:
 								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
 								GPIO_SetBits(GPIOD, GPIO_Pin_14); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD14
+							  delay_ms(4100);
 							  break;
 							case 4:
+								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
+								GPIO_SetBits(GPIOD, GPIO_Pin_15); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD15
+							  delay_ms(1000);
+							  break;
 							case 7:
 								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15); //устанавливаем низкий уровень напряжения 0 на ножку светодиода PD12/13/14/15
 								GPIO_SetBits(GPIOD, GPIO_Pin_15); //устанавливаем высокий уровень напряжения 1 на ножке светодиода PD15
+							  delay_ms(2800);
 							  break;
-							default:
-								GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
-							  break;
+//							
 						}
-						delay(47000);
-						if(tim6_count<7) tim6_count++;
-						else tim6_count=0;
-					}
-						
-				}else Flag = 0;
-			
+						Test = read_flag(data);
+						if(TIM2_count<7) TIM2_count++;
+						else TIM2_count=0;
+  	  
 		}
 		
-		
+				
 	}
 	
-	void TIM2_IRQHandler(void)
+	void SysTick_Handler(void)
 		{
+			if(SysTick_CNT > 0) SysTick_CNT--;
+			
 		}
+	
+//	void TIM2_IRQHandler(void)
+//		{
+//			if(READ_BIT(TIM2->SR, TIM_SR_UIF))
+//			{
+//				CLEAR_BIT(TIM2->SR, TIM_SR_UIF);				
+//			}
+//		}
